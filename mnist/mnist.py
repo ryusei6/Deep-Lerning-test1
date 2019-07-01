3dad6caf05aea25dbc502499b4d0cd2b2cf86120
Ryusei Kadota <45719980+ryusei6@users.noreply.github.com>
2019-07-01 23:45:21 +0900

    Update mnist.py

--- mnist/mnist.py
+++ mnist/mnist.py
@@ -2,10 +2,14 @@ import keras
 from keras.datasets import mnist
 from keras.models import Sequential
 from keras.layers import Dense
+from keras.layers.convolutional import Conv2D
+from keras.layers.pooling import MaxPool2D, GlobalAveragePooling2D
+from keras.layers.core import Activation, Flatten
+from keras.layers import BatchNormalization, Dropout
 
 batch_size = 128
 n_classes = 10
-epochs = 1
+epochs = 20
 
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 x_train = x_train.reshape(60000, 28, 28, 1)
@@ -18,11 +22,6 @@ y_train = keras.utils.to_categorical(y_train, n_classes)
 y_test = keras.utils.to_categorical(y_test, n_classes)
 
 # モデル
-from keras.layers.convolutional import Conv2D
-from keras.layers.pooling import MaxPool2D, GlobalAveragePooling2D
-from keras.layers.core import Activation, Flatten
-from keras.layers import BatchNormalization, Dropout
-
 model = Sequential()
 model.add(Conv2D(64, (3,3), padding='same', activation='relu',input_shape=(28,28,1)))
 model.add(Conv2D(64, (3,3), padding='same',activation='relu'))
