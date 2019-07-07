51faf7ffd3c07096d8b41d96fe2678dbba2ed3c4
Ryusei <e1n5te1nrrrrr@gmail.com>
2019-07-08 03:52:24 +0900

    add face_detection.py

--- scraping/scraping.py
+++ scraping/scraping.py
@@ -4,20 +4,21 @@ import urllib
 import argparse
 import requests
 from bs4 import BeautifulSoup
+import time
 
 class Google(object):
     def __init__(self):
-        self.url = "https://www.google.co.jp/search"
+        self.url = 'https://www.google.co.jp/search'
         self.session = requests.session()
         self.session.headers.update(
             {
-                "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:10.0) \
-                    Gecko/20100101 Firefox/10.0"
+                'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:10.0) \
+                    Gecko/20100101 Firefox/10.0'
             }
         )
 
     def search(self, keyword, max):
-        print('searching \"{}\"...'.format(keyword))
+        print('searching \'{}\'...'.format(keyword))
         query_generator = self._query_generator(keyword)
         return self._image_search(query_generator, max)
 
@@ -25,9 +26,9 @@ class Google(object):
         page = 0
         while page<1:
             params = urllib.parse.urlencode(
-                {"q": keyword, "tbm": "isch", "ijn": str(page)}
+                {'q': keyword, 'tbm': 'isch', 'ijn': str(page)}
             )
-            yield self.url + "?" + params
+            yield self.url + '?' + params
             page += 1
 
     def _image_search(self, query_generator, max):
@@ -36,14 +37,14 @@ class Google(object):
         while True:
             # get url
             html = self.session.get(next(query_generator)).text
-            soup = BeautifulSoup(html, "lxml")
-            elements = soup.select(".rg_meta.notranslate")
+            soup = BeautifulSoup(html, 'lxml') # lxml: C言語で高速、html.parser: Pythonで遅い
+            elements = soup.select('.rg_meta.notranslate')
             jsons = [json.loads(e.get_text()) for e in elements]
-            image_url_list = [js["ou"] for js in jsons]
+            image_url_list = [js['ou'] for js in jsons]
 
             # add search results
             if not len(image_url_list):
-                print("-> No more images")
+                print('-> No more images')
                 break
             elif len(image_url_list) > max - total:
                 results += image_url_list[: max - total]
@@ -52,15 +53,15 @@ class Google(object):
                 results += image_url_list
                 total += len(image_url_list)
 
-        print("-> Found", str(len(results)), "images")
+        print('-> Found', str(len(results)), 'images')
         return results
 
 
 def input():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-t", "--target", help="target name", type=str, required=True)
-    parser.add_argument("-n", "--number", help="number of images", type=int, required=True)
-    parser.add_argument("-d", "--directory", help="download location", type=str, default="./imgs")
+    parser.add_argument('-t', '--target', help='target name', type=str, required=True)
+    parser.add_argument('-n', '--number', help='number of images', type=int, required=True)
+    parser.add_argument('-d', '--directory', help='download location', type=str, default='./imgs')
     args = parser.parse_args()
 
     imgs_dir = args.directory
@@ -75,22 +76,23 @@ def input():
 def download(results,imgs_dir,target_name):
     download_errors = []
     for i, url in enumerate(results):
-        print("-> Downloading image", str(i + 1).zfill(4), end=" ")
+        print('-> Downloading image', str(i + 1).zfill(4), end=' ')
         try:
             urllib.request.urlretrieve(
                 url,
-                os.path.join(imgs_dir, target_name, str(i + 1).zfill(4) + ".jpg"),
+                os.path.join(imgs_dir, target_name, str(i + 1).zfill(4) + '.jpg'),
             )
-            print("successful")
+            print('successful')
+            time.sleep(1)
         except BaseException:
-            print("failed")
+            print('failed')
             download_errors.append(i + 1)
             continue
 
-    print("-" * 50)
-    print("Complete downloaded")
-    print("├─ Successful downloaded", len(results) - len(download_errors), "images")
-    print("└─ Failed to download", len(download_errors), "images", *download_errors)
+    print('-' * 50)
+    print('Complete downloaded')
+    print('├─ Successful downloaded', len(results) - len(download_errors), 'images')
+    print('└─ Failed to download', len(download_errors), 'images', *download_errors)
 
 
 def main():
