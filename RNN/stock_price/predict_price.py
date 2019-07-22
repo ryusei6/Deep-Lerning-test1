8d0c8ee8fa812d053272a7edb4f75605960b208c
Ryusei <e1n5te1nrrrrr@gmail.com>
2019-07-23 03:56:44 +0900

    modify predict_price.py

--- RNN/stock_price/predict_price.py
+++ RNN/stock_price/predict_price.py
@@ -1,33 +1,47 @@
+import keras
+import urllib
 import pandas as pd
-import stock_price_model
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler
 
 lookback = 30
+
 def get_data():
     data_file = 'nikkei_stock_average_daily_jp.csv'
     df = pd.read_csv(data_file, index_col=0, encoding='cp932', skipfooter=1, engine='python')
     closing_price = df[['終値']].values
 
-    index = -lookback
-    X_train = closing_price[index:]
-    return X_train
+    closing_price = closing_price[-lookback:]
+    latest_date = df.index.values[-1]
+    return closing_price, latest_date
+
+
+def download_stock_data():
+    url = 'https://indexes.nikkei.co.jp/nkave/historical/nikkei_stock_average_daily_jp.csv'
+    try:
+        urllib.request.urlretrieve(url,'nikkei_stock_average_daily_jp.csv')
+    except:
+        print('ファイルをダウンロードできませんでした。')
+
 
 def main():
-    X_train = get_data()
+    download_stock_data()
+
+    closing_price, latest_date = get_data()
 
     scaler = StandardScaler()
-    scaler.fit(X_train)
-    X_train_std = scaler.transform(X_train).reshape(-1, lookback, 1)
+    scaler.fit(closing_price)
+    closing_price_std = scaler.transform(closing_price).reshape(-1, lookback, 1)
 
-    model = stock_price_model.load_model()
+    model = keras.models.load_model('model.h5', compile=False)
 
-    df_predict_std = pd.DataFrame(model.predict(X_train_std), columns=['予測値'])
+    df_predict_std = pd.DataFrame(model.predict(closing_price_std), columns=['予測値'])
     predict = scaler.inverse_transform(df_predict_std['予測値'].values)
-    
-    print('------------------------------------------')
-    print('predict price: ' + str(predict[0]))
-    print('------------------------------------------')
+
+    print('\n-------------------------------------------------------------------------------')
+    print('      ('+latest_date+') Predict the next closing price(Nikkei 225): ' + str(predict[0]))
+    print('-------------------------------------------------------------------------------\n')
+
 
 if __name__ =='__main__':
     main()
--- RNN/stock_price/train_model.py
+++ RNN/stock_price/train_model.py
@@ -3,18 +3,17 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import keras
 from keras.models import Sequential
-from keras.layers import Dense, LSTM, GRU
 from keras.optimizers import RMSprop
+from keras.layers import Dense, LSTM, GRU
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import mean_squared_error
 
-data_file = 'nikkei_stock_average_daily_jp.csv'
 
 ## 予測に必要な日数
 lookback = 30
-
 epochs = 200
 
+data_file = 'nikkei_stock_average_daily_jp.csv'
 df = pd.read_csv(data_file, index_col=0, encoding='cp932', skipfooter=1, engine='python')
 closing_price = df[['終値']].values
 
@@ -37,7 +36,7 @@ def standardization(X_train, y_train, X_test):
     X_test_std = scaler.transform(X_test).reshape(-1, lookback, 1)
     scaler.fit(y_train)
     y_train_std = scaler.transform(y_train)
-    return scaler, X_train_std, X_test_std, y_train_std
+    return scaler, X_train_std, y_train_std, X_test_std
 
 
 def create_model(X_train_std):
@@ -69,11 +68,12 @@ def show_predict(model, scaler, X_test_std, y_test):
 
     ## 予測結果をプロット
     pre_date = df.index[-len(y_test):].values
-    plt.plot(pre_date, y_test, 'b', alpha=0.6, marker='.', label='raw', linewidth=1)
+    plt.figure(figsize=(10, 6))
+    plt.plot(pre_date, y_test, 'b', alpha=0.6, marker='.', label='result', linewidth=1)
     plt.plot(pre_date, predict, 'r', alpha=0.6, marker='.', label='predict', linewidth=1)
-    ticks = 30
+    ticks = 20
     plt.xticks(range(0, len(pre_date), ticks), pre_date[::ticks])
-    plt.xticks(rotation=70)
+    plt.xticks(rotation=0)
     plt.legend()
     plt.show()
 
@@ -82,29 +82,34 @@ def load_model():
     model = keras.models.load_model('model.h5', compile=False)
     return model
 
+
+def train_model(X_train_std, y_train_std):
+    model = create_model(X_train_std)
+    result = model.fit(X_train_std, y_train_std,
+                      batch_size=256,
+                      epochs=epochs,
+                      validation_split=0.1)
+    show_loss(result)
+    model.save('model.h5', include_optimizer=False)
+    return model
+
+
 def main():
     # 訓練、テスト用データ
-    test_size = 0.2
-    train_size = int(len(closing_price) * (1 - test_size))
+    train_size_ratio = 0.8
+    train_size = int(len(closing_price) * train_size_ratio)
     X_train, y_train = make_dataset(closing_price[0:train_size])
     X_test, y_test = make_dataset(closing_price[train_size:])
 
-    scaler, X_train_std, X_test_std, y_train_std = standardization(X_train, y_train, X_test)
+    scaler, X_train_std, y_train_std, X_test_std = standardization(X_train, y_train, X_test)
 
-    # 学習済みモデル
-    model = load_model()
+    # 学習済みモデルを使用
+    # model = load_model()
 
-    # 学習させる場合は下をコメントアウト
-    # model = create_model(X_train_std)
-    # result = model.fit(X_train_std, y_train_std,
-    #                   batch_size=256,
-    #                   epochs=epochs,
-    #                   validation_split=0.1)
-
-    # show_loss(result)
+    # 学習させる場合は下を実行
+    model = train_model(X_train_std, y_train_std)
 
     show_predict(model, scaler, X_test_std, y_test)
-    # model.save('model.h5', include_optimizer=False)
 
 
 if __name__ == '__main__':
