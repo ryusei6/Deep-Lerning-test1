d7508b3d20394c31f80e46ce25325847f92481ec
Ryusei <e1n5te1nrrrrr@gmail.com>
2019-07-31 23:58:08 +0900

    modify train1.py

--- TensorFlow/TensorFlow_train/train1.py
+++ TensorFlow/TensorFlow_train/train1.py
@@ -1,7 +1,10 @@
 import tensorflow as tf
+
 hello = tf.constant('Hello, TensorFlow!')
 sess = tf.Session()
 print(sess.run(hello))
+
+x = tf.constant(1.0)
 
 a = tf.constant(10)
 b = tf.constant(32)
