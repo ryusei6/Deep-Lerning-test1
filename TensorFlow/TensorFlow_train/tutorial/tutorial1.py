e4b1c2f4e9ad221757ad8b098580325c9b7cfb50
Ryusei <e1n5te1nrrrrr@gmail.com>
2019-08-03 02:14:32 +0900

    modify tutorial1.@y

--- TensorFlow/TensorFlow_train/tutorial/tutorial1.py
+++ TensorFlow/TensorFlow_train/tutorial/tutorial1.py
@@ -1,6 +1,6 @@
+import numpy as np
 import tensorflow as tf
 from tensorflow import keras
-import numpy as np
 import matplotlib.pyplot as plt
 
 # 読み込み
@@ -9,23 +9,14 @@ fashion_mnist = keras.datasets.fashion_mnist
 
 class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
                'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
-#
-# plt.figure()
-# plt.imshow(train_images[0])
-# plt.colorbar()
-# plt.gca().grid(False)
-# plt.show()
 
-plt.figure(figsize=(10,10))
-for i in range(25):
-    plt.subplot(5,5,i+1)
-    plt.xticks([])
-    plt.yticks([])
-    plt.grid(False)
-    plt.imshow(train_images[i], cmap=plt.cm.binary)
-    plt.xlabel(class_names[train_labels[i]])
+
+fig, ax = plt.subplots(5, 5, figsize=(10,10), subplot_kw=({"xticks":(), "yticks":()}))
+ax = ax.ravel()
+for i in range(len(ax)):
+    ax[i].set_xlabel(class_names[train_labels[i]])
+    ax[i].imshow(train_images[i], cmap=plt.cm.binary)
 plt.show()
-
 
 model = keras.Sequential([
     keras.layers.Flatten(input_shape=(28, 28)),
@@ -42,9 +33,6 @@ model.fit(train_images, train_labels, epochs=5)
 # 正解率の評価
 test_loss, test_acc = model.evaluate(test_images, test_labels)
 print('Test accuracy:', test_acc)
-
-predictions = model.predict(test_images)
-
 
 def plot_image(i, predictions_array, true_label, img):
     predictions_array, true_label, img = predictions_array[i], true_label[i], img[i]
@@ -77,8 +65,10 @@ def plot_value_array(i, predictions_array, true_label):
     thisplot[predicted_label].set_color('red')
     thisplot[true_label].set_color('blue')
 
-# X個のテスト画像、予測されたラベル、正解ラベルを表示します。
-# 正しい予測は青で、間違った予測は赤で表示しています。
+predictions = model.predict(test_images)
+
+# X個のテスト画像、予測されたラベル、正解ラベルを表示。
+# 正しい予測は青で、間違った予測は赤で表示
 num_rows = 5
 num_cols = 3
 num_images = num_rows*num_cols
@@ -91,13 +81,11 @@ for i in range(num_images):
 plt.show()
 
 img = test_images[0]
-print(img.shape)
 img = (np.expand_dims(img,0))
-print(img.shape)
 predictions_single = model.predict(img)
 print(predictions_single)
 plot_value_array(0, predictions_single, test_labels)
-_ = plt.xticks(range(10), class_names, rotation=45)
+_ = plt.xticks(range(10), class_names, rotation=30)
 plt.show()
 prediction = predictions[0]
-np.argmax(prediction)
+print(np.argmax(prediction))
